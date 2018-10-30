b008602c01a5241088b9f1f2e74186814214e74f
zgreathouse <greathouse.zachary@gmail.com>
2018-10-30 15:47:07 -0700

    install express, nodemon, and axios libraries, build root route, write up requests to OAuth endpoints, clean up code, add comments

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const axios = require('axios');
+
+// Config Set Up
+const appID = '4ZV3SM78DV968'; // Input your app ID here
+const appSecret = '85c407b3-46b4-bdde-a1a0-9b68e8b3022a'; // Input your app secret here
+const targetEnv = 'https://sandbox.dev.clover.com'; // Pointing to Sandbox Environment
+
+// Initialize Express
+const app = express();
+
+// Root Route
+app.get('/', (req, res) => authenticate(req, res));
+
+// Steps 1 & 2 - Request merchant authorization to receive authorization code
+const authenticate = (req, res) => {
+  const url = `${targetEnv}/oauth/authorize?client_id=${appID}`;
+
+  /* If there is no code parameter in the query string of the current url
+  redirect user for authentication. If there isn't then request API token */
+  !req.query.code ? res.redirect(url) : requestAPIToken(res, req.query)
+}
+
+// Steps 3 & 4 - Request and serve up API token using the received authorization code
+const requestAPIToken = (res, query) => {
+  const url = `${targetEnv}/oauth/token?client_id=${appID}&client_secret=${appSecret}&code=${query.code}`
+
+  // Request
+  axios.get(url)
+    .then(({ data }) => res.send(data))
+    .catch(err => res.send(err));
+}
+
+// Dynamic Port Binding
+const port = process.env.port || 5000
+app.listen(port);
