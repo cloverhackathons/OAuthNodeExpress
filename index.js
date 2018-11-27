14794181db6c7ed2068bdee77e1a454769403a20
zgreathouse <greathouse.zachary@gmail.com>
2018-11-27 10:17:56 -0800

    adjust event-stream dependency

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const axios = require('axios');
 
 // Config Set Up
-const appID = ''; // Input your app ID here
-const appSecret = ''; // Input your app secret here
-const targetEnv = 'https://sandbox.dev.clover.com'; // Pointing to Sandbox Environment
+
+// const targetEnv = 'https://sandbox.dev.clover.com'; // Pointing to Sandbox Environment
+// const targetEnv = 'https://www.clover.com'; // Pointing to Prod Environment
+
+const appID = 'PCE9V99BGBW0G'; // Input your app ID here
+const appSecret = 'be6cbf95-0cb0-7932-9662-5bfda6f0dc30'; // Input your app secret here
+const targetEnv = 'https://www.eu.clover.com'; // Pointing to EU Environment
 
 // Initialize Express
 const app = express();
@@ -13,20 +17,20 @@ const app = express();
 app.get('/', (req, res) => authenticate(req, res));
 
 // Steps 1 & 2 - Request merchant authorization to receive authorization code
-const authenticate = (req, res) => {
+const authenticate = async (req, res) => {
   const url = `${targetEnv}/oauth/authorize?client_id=${appID}`;
 
   /* If there is no code parameter in the query string of the current url
   redirect user for authentication. If there isn't then request API token */
-  !req.query.code ? res.redirect(url) : requestAPIToken(res, req.query)
+  !req.query.code ? await res.redirect(url) : await requestAPIToken(res, req.query)
 }
 
 // Steps 3 & 4 - Request and serve up API token using the received authorization code
-const requestAPIToken = (res, query) => {
+const requestAPIToken = async (res, query) => {
   const url = `${targetEnv}/oauth/token?client_id=${appID}&client_secret=${appSecret}&code=${query.code}`
 
   // Request
-  axios.get(url)
+  await axios.get(url)
     .then(({ data }) => res.send(data))
     .catch(err => res.send(err));
 }
